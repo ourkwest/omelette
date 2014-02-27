e5c2ddf11787c4c4036cede38fbabf7408b9e86b
Peter Westmacott <peter.westmacott@gmail.com>
2014-02-27 13:29:16 +0000

    Changed input UI for second pass. Finalised text.

--- src/hamlet/core.clj
+++ src/hamlet/core.clj
@@ -8,6 +8,8 @@
 (def force-update false)
 (def cut-filename "./resources/cut.txt")
 (def src-filename "./resources/hamlet.txt")
+(def wip-filename "./resources/wip.txt")
+
 
 (defn file-line-seq [filename]
   (with-open [r (reader filename)]
@@ -71,16 +73,17 @@
     (str uncut-count " / " total-count " ("  (format "%.2f" (/ uncut-count total-count 0.01)) "%)")))
 
 (defn write-out [text]
-  (spit cut-filename
+  (spit wip-filename
     (str
       "("
       (apply str (interpose "\n" (map pr-str text)))
-      ")")))
+      ")"))
+  )
 
 (defn load-parsed []
-  (if (and (not force-update) (file-exists cut-filename))
+  (if (and (not force-update) (file-exists wip-filename))
     (do
-      (def parsed-text (read-string (slurp cut-filename)))
+      (def parsed-text (read-string (slurp wip-filename)))
       (println "Read from file."))
     (do
       (def parsed-text (parse (file-line-seq src-filename)))
@@ -89,6 +92,34 @@
   (println (str "Total: " (count-lines parsed-text))))
 
 (load-parsed)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn finalise [x]
+  (let [cut (-> x :meta :cut)
+        note (-> x :meta :note)
+        notey (if (empty? note) nil note)
+        final (-> x :meta :final)
+        text (x :text)
+        new-final (or final notey (if cut nil text))
+        type (cond
+               (= (x :text) (x :act)) "A"
+               (= (x :text) (x :scene)) "S"
+               (= (x :text) (x :direction)) "D"
+               (= (x :text) (x :character)) "C"
+               :else "T")]
+    (assoc-in (assoc-in x [:meta :final] new-final) [:meta :type] type)))
+
+
+;(def parsed-text (map finalise parsed-text)) ; only done once
+;(write-out parsed-text)
+
+
+;(println (take 30 parsed-text))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -106,7 +137,7 @@
     (str \" (.replaceAll x "\"" "\"\"") \")
     ""))
 
-(defn line-to-csv [x] 
+(defn line-to-csv [x]
   (join
     ","
     (concat [(if (:cut (:meta x)) 1 0) (:line-number x)]
@@ -135,10 +166,13 @@
   (spit filename (apply str (map line-to-csv (filter (by-scene act scene) parsed-text)))))
 
 ;;(scene-to-csv ["delme.csv" "ACT I" "SCENE I"])
-(doall (map scene-to-csv scenes))
+;(doall (map scene-to-csv scenes))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (def cut-text parsed-text)
 
 (def css "<style>
@@ -162,7 +196,7 @@ html {
 
 .text {
 	margin-left: 50px;
-	width: 600px;
+	width: 450px;
 	display: inline-block;
 	color: rgb(100, 250, 100);
 //	font-weight: bold;
@@ -173,10 +207,14 @@ html {
 	font-weight: normal;
 }
 
+.tweak {
+	color: rgb(200, 200, 0);
+}
+
 .meta {
 	margin-left: 0px;
 	margin-right: 50px;
-	width: 600px;
+	width: 450px;
 	display: inline-block;
 }
 
@@ -184,9 +222,26 @@ html {
 	width: 450px;
 }
 
+.final {
+	width: 450px;
+	background-color: #000;
+	color: #fff;
+}
+
+.type {
+  width: 15px;
+}
+
 input {
-	background-color: rgb(0, 0, 0);
+  margin-right: 10px;
+  border: 1px solid grey;
+	background-color: rgb(50, 50, 50);
 	color: rgb(150, 150, 150);
+}
+input:hover {
+  border: 1px solid white;
+  background-color: rgb(0, 0, 0);
+	color: white;
 }
 input:focus {
 	background-color: rgb(50, 50, 50);
@@ -212,12 +267,37 @@ function cut(line_number) {
 
 function annotate(line_number) {
 	el = document.getElementById(\"note-\" + line_number);
-  send(\"note:\" + line_number + \":\" + el.value)
+  send(\"note:\" + line_number + \":\" + el.value);
+}
+
+function askAnnotate(line_number) {
+  el = document.getElementById(\"note-\" + line_number);
+  oldValue = el.firstChild.data;
+  newValue = prompt(\"New note:\", oldValue);
+  if (newValue && newValue != oldValue) {
+    send(\"note:\" + line_number + \":\" + newValue);
+    el.firstChild.data = newValue;
+  }
+}
+
+function finalise(line_number) {
+	el = document.getElementById(\"final-\" + line_number);
+  send(\"final:\" + line_number + \":\" + el.value);
+}
+
+function askFinalise(line_number) {
+  el = document.getElementById(\"final-\" + line_number);
+  oldValue = el.firstChild.data;
+  newValue = prompt(\"Final text:\", oldValue);
+  if (newValue && newValue != oldValue) {
+    send(\"final:\" + line_number + \":\" + newValue);
+    el.firstChild.data = newValue;
+  }
 }
 
 </script>")
 
-(def head (str "<html><head>" css js "</head><body>"))
+(def head (str "<!DOCTYPE html><html><head>" css js "</head><body>"))
 (def tail "</body></html>")
 
 
@@ -232,15 +312,26 @@ function annotate(line_number) {
 (def input (partial tag "input"))
 
 (defn web-format [line id]
-  (let [line-type (if (:ln line) "text" "meta")
-        is-cut (if (-> line :meta :cut) " cut" "")
+  (let [text (-> line :text)
         note (-> line :meta :note)
-        line-class (str line-type is-cut)]
+        final (-> line :meta :final)
+        type (-> line :meta :type)
+        line-type (if (:ln line) "text" "meta")
+        is-cut (if (-> line :meta :cut) " cut" "")
+        is-tweaked (if (not= text final) " tweak" "")]
     (str (div {:class "line"}
            (span {:class "ln"} (:ln line))
-           (span {:id id :class line-class} (:text line))
-           (input {:value "cut" :type "submit" :tabindex id :onclick (str "cut(" id ");") :onmouseover "this.focus();"} "")
-           (input {:id (str "note-" id) :class "note" :value note :type "text" :onblur (str "annotate(" id ");") } "")
+           (span {:id id :class (str line-type is-cut)} (:text line))
+           (input {:value "cut" :type "submit" :onclick (str "cut(" id ");") } "") ; :tabindex id :onmouseover "this.focus();"
+           (span {:id (str "final-" id)
+                  :class (str line-type is-cut is-tweaked)
+                  :onclick (str "askFinalise(" id ");")} (if (-> line :meta :cut) "" final))
+           (span {:id (str "note-" id)
+                  :class "note"
+                  :onclick (str "askAnnotate(" id ");")} note)
+;           (input {:id (str "type-" id) :class "type" :value type :type "text" :onblur (str "typify(" id ");") } "")
+ ;          (input {:id (str "final-" id) :class "final" :value final :type "text" :onblur (str "finalise(" id ");") } "")
+  ;         (input {:id (str "note-" id) :class "note" :value note :type "text" :onblur (str "annotate(" id ");") } "")
          ))))
 
 (defn my-page []
@@ -255,6 +346,10 @@ function annotate(line_number) {
 (defn note-data [line note]
   (let [meta (:meta line)]
     (assoc line :meta (assoc meta :note note))))
+
+(defn final-data [line final]
+  (let [meta (:meta line)]
+    (assoc line :meta (assoc meta :final final)))) ;; should use assoc-in
 
 (defn cut-line [line-number]
   (def cut-text
@@ -276,6 +371,15 @@ function annotate(line_number) {
   (write-out cut-text)
   (println (str "The note is: \"" note \")))
 
+(defn final-line [line-number final] ;; definite repetition here
+  (def cut-text
+    (concat
+      (take line-number cut-text)
+      [(final-data (nth cut-text line-number) final)]
+      (drop (inc line-number) cut-text)))
+  (write-out cut-text)
+  (println (str "The final line is: \"" final \")))
+
 (defn handle [data]
   (cond
     (.startsWith data "cut:")
@@ -285,6 +389,11 @@ function annotate(line_number) {
             id (read-string (nth bits 1))
             note (if (= (count bits) 3) (nth bits 2) "")]
         (note-line id note))
+    (.startsWith data "final:")
+      (let [bits (split data #":")
+            id (read-string (nth bits 1))
+            final (if (= (count bits) 3) (nth bits 2) "")]
+        (final-line id final))
     :else
       (println (str data "?"))))
 
